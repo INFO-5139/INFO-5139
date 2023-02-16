10087f414ba2fe2357d38356f3c7c3f72f2569a6
Dmytro Sereda <sereda21d@gmail.com>
2023-02-16 11:50:46 -0500

    First screen added

--- src/pages/HomePage/home-page.component.jsx
+++ src/pages/HomePage/home-page.component.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import {
+  HomePageContainer,
+  HeadingOne,
+  Paragraph,
+  CTAContainer,
+  PrimaryButton,
+  FirstScreenContainer,
+  CTAImage,
+  ImageContainer,
+} from "./home-page.styles";
+import Image from "../../assets/main_image.jpg";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
-  return <div>App Works</div>;
+  const navigate = useNavigate();
+  return (
+    <HomePageContainer>
+      <FirstScreenContainer>
+        <CTAContainer>
+          <HeadingOne>
+            Faux the real, go for <span>Fauxliage</span>
+          </HeadingOne>
+          <Paragraph>
+            Fake plants that look real. Now time to check new winter collection.
+            Say no to allergies and welcome new colours into your home.
+          </Paragraph>
+          <PrimaryButton
+            onClick={(e) => {
+              e.preventDefault();
+              navigate("/shop");
+            }}
+          >
+            Shop Now
+          </PrimaryButton>
+        </CTAContainer>
+        <ImageContainer>
+          <CTAImage src={Image} alt="Flower Bucket" />
+        </ImageContainer>
+      </FirstScreenContainer>
+    </HomePageContainer>
+  );
 };
 
 export default HomePage;
---
+++ src/pages/HomePage/home-page.styles.jsx
@@ -0,0 +1,63 @@
+import styled from "styled-components";
+
+export const HomePageContainer = styled.div`
+  padding: 23px 53px;
+`;
+
+export const CTAContainer = styled.div`
+  p {
+    max-width: 600px;
+    margin-bottom: 30px;
+  }
+`;
+
+export const FirstScreenContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const HeadingOne = styled.h1`
+  font-weight: 700;
+  font-size: 50px;
+  color: #333;
+  max-width: 700px;
+  line-height: 1;
+  margin-bottom: 30px;
+
+  span {
+    color: #388b37;
+  }
+`;
+
+export const Paragraph = styled.p`
+  font-size: 20px;
+`;
+
+export const PrimaryButton = styled.button`
+  border: none;
+  background-color: #388b37;
+  color: #fff;
+  border-radius: 30px;
+  padding: 14px 70px;
+  font-size: 24px;
+  cursor: pointer;
+`;
+
+export const ImageContainer = styled.div`
+  overflow: hidden;
+  max-width: 450px;
+  box-shadow: -20px 20px 3px rgba(0, 0, 0, 0.25);
+`;
+
+export const CTAImage = styled.img`
+  width: 100%;
+  height: 100%;
+  display: block;
+  object-fit: cover;
+  transition: all 0.2s;
+
+  &:hover {
+    transform: scale(1.05);
+  }
+`;
