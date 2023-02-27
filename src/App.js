85b5dc5f32482a4458cfd761811de608edb8cd23
William <williamw15@protonmail.com>
2023-02-27 08:29:13 -0500

    correct routing syntax error

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import HomePage from './pages/HomePage/home-page.component';
 import Header from './components/header/header.component';
 import Footer from './components/footer/footer.component';
 import Login from './pages/Login/login-component';
+import SignUp from './pages/SignUp/signup-component';
 
 function App() {
   return (
@@ -20,6 +21,10 @@ function App() {
         <Route
           path='login'
           element={<Login />}
+        />
+        <Route
+          path='signup'
+          element={<SignUp />}
         />
       </Routes>
       <Footer />
