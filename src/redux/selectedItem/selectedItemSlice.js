a5b34875fc9f5167a8408aed714857eeabbb8245
William <williamw15@protonmail.com>
2023-04-12 10:46:30 -0400

    selected item is passed to redux store via selectedItemSlice

---
+++ src/redux/selectedItem/selectedItemSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  item: '',
+};
+
+export const selectedItemSlice = createSlice({
+  name: 'selectedItem',
+  initialState,
+  reducers: {
+    setSelectedItem: (state, action) => {
+      state.item = action.payload;
+    },
+  },
+});
+
+export const { setSelectedItem } = selectedItemSlice.actions;
+export default selectedItemSlice.reducer;
