777017c270827b9e8b672e13c03a766e70f4a594
anti-spades <a_valsamos@fanshaweonline.ca>
2023-04-14 13:02:40 -0400

    adding email sign up

--- src/pages/Account/account-styles.jsx
+++ src/pages/Account/account-styles.jsx
@@ -9,5 +9,37 @@ export const PrimaryButton = styled.button`
   font-size: 24px;
   cursor: pointer;
   margin-top: 10%;
-  margin-bottom: 20%;
+  margin-bottom: 10%;
+  margin-left: 2%;
+`;
+
+
+export const SubmitButton = styled.div`
+background-color: ${(props) => props.colors.solidButtonColor};
+border-radius: 25px;
+border: none;
+padding: 0.8rem 0 0.8rem 0;
+color: ${(props) => props.colors.solidButtonColorText};
+margin-left: 6vw;
+font-size: 18px;
+text-decoration: none;
+cursor: pointer;
+// margin-bottom: 120%;
+width: 10%;
+text-align: center;
+margin-top: 2vw;
+margin-bottom: 2vw;
+`;
+
+export const FormStyle = styled.div`
+color: ${(props) => props.colors.textColor};
+margin-left: 2vw;
+label {
+  display: block;
+  font-weight: bold;
+}
+p{
+  margin-top: 2vw;
+  margin-bottom: 1vw;
+}
 `;
--- src/pages/Account/account.jsx
+++ src/pages/Account/account.jsx
@@ -1,9 +1,11 @@
 import { auth } from './../../api/firebaseConfig';
-import { PrimaryButton } from './account-styles';
-import React, { useState } from 'react';
+import { PrimaryButton, SubmitButton, FormStyle } from './account-styles';
+import React, { useRef } from 'react';
+
 import { useSelector } from 'react-redux';
 const Account = () => {
   const themeColor = useSelector((state) => state.theme);
+  const form = useRef();
   const DeleteUser = () => {
     const user = auth.currentUser;
     user
@@ -17,15 +19,38 @@ const Account = () => {
         // ...
       });
   };
+
+  const signUp = () => {
+    console.log("Signed up for emails!")
+  }
+
   return (
-    <div>
+    <><div>
       <PrimaryButton
         colors={themeColor}
         onClick={DeleteUser}
       >
         Delete Account
       </PrimaryButton>
+      <FormStyle colors = {themeColor}>
+      <h1>Be the first to know!</h1>
+      <p>Sign up to our email listing to know about
+        <br></br>Up coming deals, new items and more!</p>
+      <form
+      ref={form}
+      onSubmit={signUp}
+    >
+
+        <input
+          type='text'
+          name='email' />
+      </form>
+      </FormStyle>
+      <SubmitButton
+        colors={themeColor}
+      >Sign up!</SubmitButton>
     </div>
+    </>
   );
 };
 
