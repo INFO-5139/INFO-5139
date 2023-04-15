be350802ea093fff7b1d41ca81e50be8d0766244
William <williamw15@protonmail.com>
2023-04-15 11:11:51 -0400

    add payment form field with state management

--- src/components/payment/payment.component.jsx
+++ src/components/payment/payment.component.jsx
@@ -1,14 +1,67 @@
 import { useState } from 'react';
 
+import { InputField, PaymentFormWrapper } from './payment.styles';
+
 const Payment = () => {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [creditCardNumber, setCreditCardNumber] = useState('');
   const [securityCode, setSecurityCode] = useState('');
-  const [expirationDate, setExpirationDate] = useState('');
+  const [expirationDateMonth, setExpirationDateMonth] = useState('');
+  const [expirationDateYear, setExpirationDateYear] = useState('');
+
   return (
     <>
-      <div>Payment renders</div>
+      <PaymentFormWrapper>
+        <label>
+          First Name
+          <InputField
+            type='text'
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+        </label>
+        <label>
+          Last Name
+          <InputField
+            type='text'
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </label>
+        <label>
+          Credit Card Number
+          <InputField
+            type='number'
+            value={creditCardNumber}
+            onChange={(e) => setCreditCardNumber(e.target.value)}
+          />
+        </label>
+        <label>
+          Security Code
+          <InputField
+            type='number'
+            value={securityCode}
+            onChange={(e) => setSecurityCode(e.target.value)}
+          />
+        </label>
+        <label>
+          Month
+          <InputField
+            type='number'
+            value={expirationDateMonth}
+            onChange={(e) => setExpirationDateMonth(e.target.value)}
+          />
+        </label>
+        <label>
+          Year
+          <InputField
+            type='number'
+            value={expirationDateYear}
+            onChange={(e) => setExpirationDateYear(e.target.value)}
+          />
+        </label>
+      </PaymentFormWrapper>
     </>
   );
 };
---
+++ src/components/payment/payment.styles.jsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const PaymentFormWrapper = styled.form`
+  margin-top: 2rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+export const InputField = styled.input`
+  width: 100%;
+  //   width: 30vw;
+  padding: 0.3rem 0 0.3rem 0;
+  margin-bottom: 2rem;
+`;
